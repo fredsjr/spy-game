02fe03e606aa2e9cf61fea56b40a359adcd04f23
fredsjr <Oli4schell@gmail.com>
2023-05-16 15:13:29 +0200

    added new background, music and title screen

--- src/js/game.js
+++ src/js/game.js
@@ -1,22 +1,57 @@
-import '../css/style.css'
-import { Actor, Engine, Vector } from "excalibur"
-import { Resources, ResourceLoader } from './resources.js'
+import { Actor, Engine, Vector, Label, Color, Font, vec, Sound } from "excalibur";
+
+import { Resources, ResourceLoader } from "./resources.js";
 
 export class Game extends Engine {
-
     constructor() {
-        super({ width: 800, height: 600 })
-        this.start(ResourceLoader).then(() => this.startGame())
+        super({ width: 1630, height: 830, backgroundColor: Color.Black });
+        this.start(ResourceLoader).then(() => this.startGame());
     }
 
     startGame() {
-        console.log("start de game!")
-        const fish = new Actor()
-        fish.graphics.use(Resources.Fish.toSprite())
-        fish.pos = new Vector(400, 300)
-        fish.vel = new Vector(-10,0)
-        this.add(fish)
+        Resources.music.play(0.7)
+
+        let background = new Actor({
+            pos: new Vector(0, 0),
+            width: 800, height: 500,
+            anchor: vec(0, 0)
+        })
+        background.graphics.use(Resources.fort.toSprite())
+        this.add(background)
+
+        for (let i = 0; i < 10; i++) {
+            const Spycrab = new Actor({
+                width: Resources.spyCrab.width,
+                height: Resources.spyCrab.height,
+            });
+            Spycrab.graphics.use(Resources.spyCrab.toSprite());
+
+            // const x = this.halfDrawWidth
+            Spycrab.pos = new Vector(
+                Math.random() * this.drawWidth,
+                Math.random() * this.drawHeight
+            );
+            Spycrab.vel = new Vector(Math.random() * 50, 0);
+            this.add(Spycrab);
+
+            Spycrab.on("pointerdown", (event) => {
+                console.log("You are dead, no big suprise");
+                Spycrab.kill();
+            });
+            Spycrab.on("pointerdown", (dead) => {
+                console.log('lol')
+                Resources.death.play(0.7);
+            });
+            background.on("pointerdown", (piss) => {
+                console.log('lol')
+                Resources.piss.play(0.7);
+            });
+            this.add(Spycrab);
+
+            const sc = 0.2;
+            Spycrab.scale = new Vector(sc, sc);
+        }
     }
 }
 
-new Game()
+new Game();
--- src/js/resources.js
+++ src/js/resources.js
@@ -1,9 +1,33 @@
-import { ImageSource, Sound, Resource, Loader } from 'excalibur'
-import fishImage from '../images/fish.png'
+import { ImageSource, Sound, Resource, Loader, Color } from "excalibur";
+import spyCrab from "../images/Spycrab.png";
+import fort from "../images/2fort.png";
+import scope from "../images/sniper_scope.png";
+import music from "../sounds/french-meme-song.mp3";
+import death from "../sounds/spy-death-sound.mp3";
+import piss from "../sounds/piss-sniper-sound-effect.mp3"
+import titleImage from "../images/game-logo.png"
 
 const Resources = {
-    Fish: new ImageSource(fishImage)
-}
-const ResourceLoader = new Loader([Resources.Fish])
+    spyCrab: new ImageSource(spyCrab),
+    fort: new ImageSource(fort),
+    scope: new ImageSource(scope),
+    music: new Sound(music),
+    death: new Sound(death),
+    piss: new Sound(piss),
+    logo: new ImageSource(titleImage)
+};
+const ResourceLoader = new Loader([
+    Resources.spyCrab,
+    Resources.fort,
+    Resources.scope,
+    Resources.music,
+    Resources.death,
+    Resources.piss,
+    Resources.logo
+]);
+ResourceLoader.logo = titleImage
+ResourceLoader.logoWidth = 900
+ResourceLoader.logoHeight = 450
+ResourceLoader.backgroundColor = Color.White
 
-export { Resources, ResourceLoader }
+export { Resources, ResourceLoader };
